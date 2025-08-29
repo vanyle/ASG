8e856cbe90c13d83e8bf1078e1b9198154466012
Antoine Delègue <delegue.antoine@gmail.com>
2025-08-30 00:10:28 +0200

    :wrench: also make windows script target the latest version

--- install/get_asg_win.ps1
+++ install/get_asg_win.ps1
@@ -1,13 +1,19 @@
 #!/usr/bin/env pwsh
 
+$start_path = $PWD
+
 Write-Output "Installing ASG"
 cd $HOME
-Invoke-WebRequest "https://github.com/vanyle/ASG/releases/download/0.2.2/asg-0.2.2-windows-amd64.tar.gz" -OutFile asg.tar.gz
+
+$download_url = (Invoke-WebRequest "https://api.github.com/repos/vanyle/asg/releases/latest" | select -ExpandProperty "content" | ConvertFrom-Json | select -ExpandProperty "assets" | select -ExpandProperty "browser_download_url" |  Select-String -Pattern "windows").toString()
+
+Invoke-WebRequest $download_url -OutFile asg.tar.gz
 tar xzf asg.tar.gz
 Move-Item -Path build -Destination .asg
 Remove-Item -Path asg.tar.gz
 
 $env:Path += ";$HOME\.asg"
 
+cd $start_path
 Write-Output "You're all set! You can now run 'asg' to see the available commands"
 Write-Output "You might want to add $HOME\.asg to your PATH"
